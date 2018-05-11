e2b8f6941b6579a22d77b99bee95734cff6b4b97
jamesstefens18 <fireisaverywickedmaster@gmail.com>
2018-05-11 20:30:30 +0000

    "changed to src"

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/src"));
 
 var db;
 
@@ -32,5 +32,5 @@ function handleError(res, reason, message, code){
 
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'))
+    res.sendFile(path.join(__dirname+'/src/index.html'))
 })
